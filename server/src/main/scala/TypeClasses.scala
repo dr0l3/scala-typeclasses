df8c2acad53b9caf94f000915b4b497f1b63199a
dr0l3 <runedrolepedersen@gmail.com>
2018-08-05 10:41:55 +0200

    Omg

--- server/src/main/scala/TypeClasses.scala
+++ server/src/main/scala/TypeClasses.scala
@@ -1,16 +1,15 @@
 import diesel._, cats._, cats.implicits._
 import simulacrum._
 
-object DieselDemo {
+object DieselDemo extends App {
 
   @typeclass trait Combineable[A] {
-    @op("|+|") def combine(x: A, y: A): A
+    @op("|~|") def combinez(x: A, y: A): A
   }
 
   implicit val combinedInt: Combineable[Int] = new Combineable[Int] {
-    def combine(x: Int, y: Int) = x + y
+    def combinez(x: Int, y: Int) = x + y
   }
-
 
   @diesel
   trait Maths[F[_]] {
@@ -18,13 +17,16 @@ object DieselDemo {
     def add(l: Int, r: Int): F[Int]
   }
 
+
   @diesel
   trait Logger[F[_]] {
     def info(s: String): F[Unit]
   }
 
+
   // Import companion-to-interpreter aliasing sugar
   import Maths.ops._, Logger.ops._
+  import Combineable.ops._
 
   def prog[F[_]: Monad: Maths: Logger](x: Int, y: Int): F[Int] = {
     for {
@@ -34,22 +36,20 @@ object DieselDemo {
       _ <- Logger.info(s"Sum: $s")
       f <- Maths.add(p, s)
       _ <- Logger.info(s"Final: $f")
-      _ <- Logger.info(s"OMG: ${p |+| s}")
+      lol = p |~| s
+      _ <- Logger.info(s"OMG: ${lol}")
     } yield f
   }
 
-  def main(args: Array[String]): Unit = {
-
-    // Wire our interpreters in
-    implicit val mathsInterp = new Maths[Id] {
-      def times(l: Int, r: Int) = l * r
-      def add(l: Int, r: Int) = l + r
-    }
-    implicit val loggingInterp = new Logger[Id] {
-      def info(msg: String) = println(msg)
-    }
-
-    val _ = prog[Id](3, 4)
+  // Wire our interpreters in
+  implicit val mathsInterp = new Maths[Id] {
+    def times(l: Int, r: Int) = l * r
+    def add(l: Int, r: Int) = l + r
   }
+  implicit val loggingInterp = new Logger[Id] {
+    def info(msg: String) = println(msg)
+  }
+
+  val _ = prog[Id](3, 4)
 
 }
